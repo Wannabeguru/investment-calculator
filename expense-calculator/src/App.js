fd70e84dfe7be5c0ee75298edbfbb074b0892f87
Ophir Bentov <wannabegurux@gmail.com>
2023-06-29 15:28:58 -0700

    added submitHandler and further organized components.

--- expense-calculator/src/App.js
+++ expense-calculator/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/UserInput/form";
+import Header from "./components/Header/header";
+import ResultTable from "./components/ResultsTable/results";
 
 function App() {
+  const calculateHandler = (userInput) => {
+
+    const yearlyData = []; // per-year results
+
+    let currentSavings = +userInput["current-savings"]; 
+    const yearlyContribution = +userInput["yearly-contribution"]; 
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+
+    // do something with yearlyData ...
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Form />
+      {/* Todo: Show below table conditionally (only once result data is available) */}
+      {/* Show fallback text if no data is available */}
+      <ResultTable />
     </div>
   );
 }
